e8b2dbf409cff5b27aa6f98359e9882926272ba0
xsa <i@alekseysavin.com>
2021-11-13 13:32:55 +0300

    make install command for build in grpc-stack

---
+++ make_helloword.sh
@@ -0,0 +1,12 @@
+# Build helloworld example using cmake
+# mkdir -p "examples/cpp/helloworld/cmake/build"
+# pushd "examples/cpp/helloworld/cmake/build"
+# cmake ../..
+# make
+# popd
+
+mkdir -p "/home/xsa/DEV/xsa-dev/grpc/examples/cpp/vcmi/cmake/build"
+cd "/home/xsa/DEV/xsa-dev/grpc/examples/cpp/vcmi/cmake/build"
+cmake ../..
+make
+
